7e4e5ce05306332548eeec2d9e0808cd7ed39076
Kohei Yamamoto <kymmt90@gmail.com>
2018-07-10 00:01:41 +0900

    Add columns to tasks

--- db/migrations/20180709145003_create_tasks.rb
+++ db/migrations/20180709145003_create_tasks.rb
@@ -3,6 +3,9 @@ Hanami::Model.migration do
     create_table :tasks do
       primary_key :id
 
+      column :name, String, null: false
+      column :description, String, null: false
+
       column :created_at, DateTime, null: false
       column :updated_at, DateTime, null: false
     end
