5bbc3f8bb9eef79caed6a5bddb900df2ed942752
Kohei Yamamoto <kymmt90@gmail.com>
2018-07-12 00:45:17 +0900

    Add tasks#edit and tasks#update

--- apps/web/controllers/tasks/update.rb
+++ apps/web/controllers/tasks/update.rb
@@ -2,7 +2,31 @@ module Web::Controllers::Tasks
   class Update
     include Web::Action
 
+    expose :task
+
+    params do
+      required(:task).schema do
+        required(:name).filled
+        required(:description).filled
+      end
+    end
+
     def call(params)
+      @task = repository.find(params[:id])
+
+      if params.valid?
+        repository.update(params[:id], params[:task])
+
+        redirect_to routes.tasks_path
+      else
+        self.status = 422
+      end
+    end
+
+    private
+
+    def repository
+      @repository ||= TaskRepository.new
     end
   end
 end
--- apps/web/views/tasks/update.rb
+++ apps/web/views/tasks/update.rb
@@ -1,5 +1,6 @@
 module Web::Views::Tasks
   class Update
     include Web::View
+    template 'tasks/edit'
   end
 end
