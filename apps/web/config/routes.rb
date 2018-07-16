156b34fd644da9bf1e3436bb655d0b2a8df5b168
Kohei Yamamoto <kymmt90@gmail.com>
2018-07-16 16:55:01 +0900

    Add tasks#show

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -1,2 +1,1 @@
-resources :tasks, only: [:index, :new, :create, :edit, :update, :destroy]
-get '/tasks/:id', to: 'tasks#show'
+resources :tasks
--- apps/web/controllers/tasks/show.rb
+++ apps/web/controllers/tasks/show.rb
@@ -2,7 +2,10 @@ module Web::Controllers::Tasks
   class Show
     include Web::Action
 
+    expose :task
+
     def call(params)
+      @task = TaskRepository.new.find(params[:id])
     end
   end
 end
